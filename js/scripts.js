97d45b7eb156c592c8f077e2f3a87653bf6dcd1a
kenjiro okada <124141958+okaken3227@users.noreply.github.com>
2024-09-18 21:42:51 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 // Smooth Scroll for navigation links
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+document.querySelectorAll("nav a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
     e.preventDefault();
-    const targetId = this.getAttribute('href').substring(1);
+    const targetId = this.getAttribute("href").substring(1);
     const targetSection = document.getElementById(targetId);
-    
+
     targetSection.scrollIntoView({
-      behavior: 'smooth'
+      behavior: "smooth",
     });
   });
+});
+
+// Hamburger menu toggle
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navbar-nav");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
